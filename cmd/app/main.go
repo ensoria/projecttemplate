8ef9e2464c0b8525c732cb67b6540ec72dbc65bd
ichikawa <68413973+yrichika@users.noreply.github.com>
2026-01-28 08:13:39 +0900

    refactor: replace flag package with pflag for command-line argument parsing

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/ensoria/projecttemplate/internal/app/bootstrap/scheduler"
 	"github.com/ensoria/projecttemplate/internal/app/bootstrap/server"
+	"github.com/spf13/pflag"
 )
 
 func main() {
-
 	// FIXME: configのenvを使って、ここのリストを修正する
 	// envList := slices.Join(env.StringList, ", ")
-	envVal := flag.String("e", "local", "it must be either [local], [develop], [staging], [production] or [testing].")
-	isScheduler := flag.Bool("scheduler", false, "if true, run as scheduler.")
-	flag.Parse()
+	envVal := pflag.StringP("env", "e", "local", "it must be either [local], [develop], [staging], [production] or [testing].")
+	isScheduler := pflag.BoolP("scheduler", "s", false, "if true, run as scheduler.")
+	pflag.Parse()
+
 	if envVal == nil {
 		panic("Please specify the environment with -e option. It must be either [local], [develop], [staging], [production] or [testing].")
 	}
--- go.mod
+++ go.mod
@@ -28,6 +28,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.32 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
 	modernc.org/libc v1.66.10 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
